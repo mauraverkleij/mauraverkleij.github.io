1e1f0b2bed97246495f853e5d980e2c124a72038
Tim Noorlander <tgnoorlander@gmail.com>
2017-10-09 21:09:02 +0200

    added build stuff

--- src/data.js
+++ src/data.js
@@ -51,7 +51,32 @@ const data = {
             },
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
+        {
+            id: "seks-hihi",
+            title: "Seks, hihi",
+            content: "Het stimuleren van veilige seks begint bij goede voorlichting. ‘Seks, hihi’ brengt onze puberale reactie op seksuele voorlichting in beeld.\n Deze poster was een aantal maanden te vinden in de Affiche Gallerij & 3sec.gallery. In de periode rond wereld Aidsdag (1 december 2017) wordt mijn ontwerp verspreid als Boomerang ansichtkaart. Daarnaast zal ‘Seks, hihi’ begin 2018 gebruikt worden op de condoom verpakkingen die door de GGD Haaglanden verspreid gaan worden.",
+            image: {
+                src: root + "img/MauraVerkleij-Portfolio-seks-hihi0.jpg",
+                alt: "Grafisch poster ontwerp door Maura Verkleij over het onderwerp veilige seks"
+            },
+            images: [
+                {
+                    src: root + "img/MauraVerkleij-Portfolio-seks-hihi0.jpg",
+                    alt: "Grafisch poster ontwerp door Maura Verkleij over het onderwerp veilige seks",
+                    content: "eventueel tekst voor onder afbeelding"
+                },
+                {
+                    src: root + "img/MauraVerkleij-Portfolio-seks-hihi1.jpg",
+                    alt: "Grafisch poster ontwerp door Maura Verkleij over het onderwerp veilige seks tentoongesteld in de 3sec.gallery."
+                },
+                {
+                    src: root + "img/MauraVerkleij-Portfolio-seks-hihi2.jpg",
+                    alt: "Condoom ontwerp voor GGD Haaglanden"
+                }
+            ]
+        },
     ]
 };
+
 
 export default data;
